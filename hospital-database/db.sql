363d64cc8949faac51200fd6bde511c5ccb2c00e
códy <cob322@lehigh.edu>
2022-07-22 19:07:32 -0400

    Build Database container and import schema (#9)
    View ```runme``` for how to run container locally.

---
+++ hospital-database/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Patient_info(PATIENTname VARCHAR(50), PATIENTage int UNSIGNED, phoneNumber bigint UNSIGNED, patientID int PRIMARY KEY AUTO_INCREMENT);
+CREATE TABLE Patient_Roominfo(PATIENTname VARCHAR(50), RoomNumber int UNSIGNED, Department VARCHAR(50));
+CREATE TABLE Nurse(NURSEname VARCHAR(50), NURSEphone bigint UNSIGNED, NURSEfield VARCHAR(50),  NURSEdaysPresent VARCHAR(50), NURSEtimePresent VARCHAR(50) );
+CREATE TABLE Patient_Medicines(PATIENTname VARCHAR(50), MedicineName VARCHAR(20), MedicineDose VARCHAR (20));
+CREATE TABLE Patien_DAIGNOSTICS(PATIENTname VARCHAR(50), procedureRoomNumber int UNSIGNED, ProcedureRAN VARCHAR(50));
+CREATE TABLE Patien_ExaminationResult(PATIENTname VARCHAR(50),  medicalissues VARCHAR(50), DepartmentAssigned VARCHAR(50));
+CREATE TABLE Ambulance(DriverName VARCHAR(50), AmbulanceNumber int UNSIGNED, AmbulanceDaysAvalible VARCHAR(60), AmbulanceTimeAvaliable VARCHAR(60));
+CREATE TABLE cleaningStaff(StaffName VARCHAR(50), StaffDaysAvaliable VARCHAR(70), StaffTimeAvaliable VARCHAR(20), StaffCleaningArea VARCHAR(30));
+CREATE TABLE BillingInfo(PATIENTname VARCHAR(50), MoneyOwed int UNSIGNED, MoneyDueBy VARCHAR(50));
